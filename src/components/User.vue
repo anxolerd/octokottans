de115d67fb4c916625067a31d85d21aeae409c24
Oleksandr Kovalchuk <anxolerd@outlook.com>
2017-09-03 23:12:31 +0300

    Add user data fetching logic

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="page">
     <div class="header">
-      <h1 class="header__text">{{ username }}</h1>
+      <h1 class="header__text" v-if="this.user">
+        {{ this.user.name }}
+        ({{ this.user.login }})
+      </h1>
     </div>
 
     <main class="main">
-
+      <ul>
+        <li v-for="repo in this.repos">
+          {{repo.name}}
+        </li>
+      </ul>
+      <div v-if="hasNext">
+        <button role="button" class="button" @click.prevent="onLoadMoreClick">
+          <span class="button__text">Load more</span>
+        </button>
+      </div>
     </main>
   </div>
 </template>
@@ -15,8 +27,38 @@
 export default {
   name: 'user',
   props: ['username'],
+
+  created() {
+    this.$github.user(this.username, this.userCb.bind(this));
+    this.$github.userRepos(this.username, this.page, this.reposCb.bind(this));
+  },
+
   data() {
     return {
+      user: null,
+      repos: [],
+      page: 1,
+      hasNext: true,
+
+      onLoadMoreClick() {
+        this.$github.userRepos(
+          this.username,
+          this.page,
+          this.reposCb.bind(this),
+        );
+      },
+
+      reposCb(data, err) {
+        if (err) { throw err; }
+        this.hasNext = data.length > 0;
+        data.forEach((repo) => { this.repos.push(repo); });
+        this.page += 1;
+      },
+
+      userCb(data, err) {
+        if (err) { throw err; }
+        this.user = data;
+      },
     };
   },
 };
